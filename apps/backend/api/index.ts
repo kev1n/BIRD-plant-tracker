947fa02f3562fa9fabf57ced113cf2d10b2a1a13
Kevin Wang <kenwang3333@gmail.com>
2025-03-01 15:33:55 -0600

    PLEASE

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -2,7 +2,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, NextFunction, Request, Response } from 'express';
-import authRoutes from './routes/authRoutes.js';
+import authRoutes from '../routes/authRoutes.js';
 
 dotenv.config();
 
