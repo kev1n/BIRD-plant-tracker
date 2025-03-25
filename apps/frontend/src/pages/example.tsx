257388eb85df053f7ce7703a50a99bf94ef9d496
Kevin Wang <kenwang3333@gmail.com>
2025-03-25 17:04:24 -0400

    feat: create modified alerts

---
+++ apps/frontend/src/pages/example.tsx
@@ -0,0 +1,11 @@
+import { ButtonExample } from "../components/ui/examples/ButtonExample";
+import { CardExample } from "../components/ui/examples/CardExample";
+
+export default function ExamplePage() {
+  return (
+    <div className="">
+      <ButtonExample />
+      <CardExample />
+    </div>
+  );
+} 
