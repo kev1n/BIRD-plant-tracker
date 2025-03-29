c8a33a1f85367290c6b3a2697ba4b256aeafd517
Tree-0 <117487029+Tree-0@users.noreply.github.com>
2025-03-29 13:15:00 -0500

    feat: plant selector post/del/put endpoints

--- apps/backend/controllers/plantController.ts
+++ apps/backend/controllers/plantController.ts
@@ -1,6 +1,157 @@
 import { Request, Response } from 'express';
 import supabase from '../config/supabase.js';
+import { UpdatePlantBody } from '../types.js';
 
+export async function postPlant(req: Request, res: Response) {
+    try {
+        // TODO: authentication and authorization
+
+        // plant info
+        const { plantCommonName, plantScientificName, isNative, subcategory } = req.body;
+            
+        if (!plantCommonName) {
+            // missing the only required field
+            res.status(400).json({ message: 'Bad request: Plant common name is required.'});
+            return;
+        }
+
+        // insert plant
+        const { data: plantID, error: plantError } = await supabase
+            .from('PlantInfo')
+            .insert([
+                {
+                    plantCommonName: plantCommonName,
+                    plantScientificName: plantScientificName || null,
+                    isNative: isNative || null,
+                    subcategory: subcategory || null,
+                },
+            ])
+            .select('plantID')
+            .single();
+
+        if (plantError) {
+            // error during insertion
+            res.status(400).json({ message: `Bad request: ${plantError.message}` });
+            return;
+        }
+
+        if (!plantID) {
+            // data from insertion is missing
+            res.status(400).json({
+                message: `Expected plantID from insertion, recieved ${plantID}`
+            });
+            return;
+        }
+
+        res.status(201).json({ 
+            message: 'Plant successfully added.', 
+            plantID: plantID, 
+        });
+    } 
+    catch (error) {
+        res.status(500).json({ message: `Internal server error: ${error}` });
+        return;
+    }
+}
+
+export async function deletePlant(req: Request, res: Response) {
+    try {
+        // TODO : authentication and authorization 
+
+        const { plantID } = req.params;
+
+        if (!plantID) {
+            // plantID not given
+            res.status(400).json({ message: 'Missing plant ID.' });
+            return;
+        }
+
+        const currentTimestampz = new Date().toISOString(); // current time
+
+        const { data, error } = await supabase // find plant to make sure it exists
+            .from('PlantInfo')
+            .update({ deletedOn: currentTimestampz })
+            .eq('plantID', plantID)
+            .select();
+        
+        if (error) {
+            // error during soft delete
+            res.status(400).json({ error: error.message });
+            return;
+        }
+
+        if (data.length == 0) {
+            // plant does not exist
+            res.status(400).json({ error: `Plant ID: ${plantID} is not valid` });
+            return;
+        }
+
+        res.status(200).json({
+            message: 'Plant deleted successfully',
+            deletedOn: currentTimestampz,
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: `Internal server error: ${error}` });
+    }
+}
+
+export async function updatePlant(req: Request, res: Response) {
+    try {
+        // TODO: authentication and authorization
+
+        const { plantID } = req.params;
+        const { plantCommonName, plantScientificName, isNative, subcategory } = req.body;
+
+        if (!plantID) {
+            // missing path param or required name field
+            res.status(400).json({ message: 'Missing plant ID' });
+            return;
+        }
+
+        const updates: UpdatePlantBody = {};
+
+        // only update provided values
+        if (plantCommonName !== undefined) updates.plantCommonName = plantCommonName;
+        if (plantScientificName !== undefined) updates.plantScientificName = plantScientificName;
+        if (isNative !== undefined) updates.isNative = isNative;
+        if (subcategory !== undefined) updates.subcategory = subcategory;
+
+        if (Object.keys(updates).length === 0) {
+            res.status(400).json({ error: 'No fields provided to update' });
+            return;
+        }
+
+        const { data, error} = await supabase
+            .from('PlantInfo')
+            .update(updates)
+            .eq('plantID', plantID)
+            .select()
+            .maybeSingle();
+        
+        console.log(plantID);
+        console.log(data);
+        const user = await supabase.auth.getUser();
+        console.log('Current user:');
+        console.log(user);
+
+
+        if (error) {
+            res.status(400).json({ error: error.message });
+            return;
+        }
+
+        if (!data) {
+            // data is null if no row is updated or found
+            res.status(404).json({ error: 'No matching row updated' });
+            return;
+        }
+
+        res.status(200).json({ message: 'Plant updated successfully '});
+    } catch (error) {
+        res.status(500).json({ error: `Internal server error: ${error}` });
+    }
+}
 
 export async function getPlant(req: Request, res: Response)
 {
--- apps/backend/routes/plantRoutes.ts
+++ apps/backend/routes/plantRoutes.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 import * as plantController from '../controllers/plantController.js';
-//import authMiddleware from '../middleware/authMiddleware.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const plantRoutes: Router = express.Router();
 
-// route param
+plantRoutes.post('/', authMiddleware, plantController.postPlant);
+plantRoutes.delete('/:plantID', authMiddleware, plantController.deletePlant);
+plantRoutes.put('/:plantID', authMiddleware, plantController.updatePlant);
 plantRoutes.get('/:plantID', plantController.getPlant);
 //  plants?name=partialName
 //  search string is query param
