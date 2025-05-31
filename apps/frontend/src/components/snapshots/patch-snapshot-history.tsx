5d6bdf3612e2f58da1c0c91babd68397bfeb5e5b
jiayi-jenny-zhou <zhou_jiayi05@outlook.com>
2025-05-31 12:22:29 -0500

    quick fix

--- apps/frontend/src/components/snapshots/patch-snapshot-history.tsx
+++ apps/frontend/src/components/snapshots/patch-snapshot-history.tsx
@@ -26,6 +26,7 @@ export default function PatchSnapshotHistory({
   trigger?: ReactNode;
 }) {
   const [historicalSnapshots, setHistoricalSnapshots] = useState<DateIDPair[]>([]);
+  const [open, setOpen] = useState(false);
 
   const fetchHistoricalSnapshotMetadata = async (patch: string) => {
     try {
@@ -60,7 +61,7 @@ export default function PatchSnapshotHistory({
 
   useEffect(() => {
     fetchHistoricalSnapshotMetadata(patch);
-  }, [patch]);
+  }, [patch, open]);
 
   return (
     <HistoricalSnapshotContext.Provider
@@ -68,7 +69,7 @@ export default function PatchSnapshotHistory({
         fetchHistoricalSnapshotMetadata,
       }}
     >
-      <Dialog>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
           {trigger || <Button variant="outline">History</Button>}
         </DialogTrigger>
